6493f8dac319b570ecbc222d4a01e83fcdd3edeb
Valeron01 <electronpochta3011@mail.ru>
2022-04-24 11:05:17 +0300

    Добавил страницу логина

--- triangle/frontend/static/js/index.js
+++ triangle/frontend/static/js/index.js
@@ -1,8 +1,28 @@
 window.onload =
-function (){
-    $("#register_button").click(function () {
-        $("#choose_auth_type").css(
-            "display", "none"
-        );
+function ()
+{
+    function changeDisplay(object, vis = "none") {
+        return function () {
+            $(object).css(
+                "display", vis
+            );
+        };
+    }
+
+    $("#register_button").click(changeDisplay("#choose_auth_type", "none"));
+
+    $("#auth_button").click(function (){
+        changeDisplay("#choose_auth_type", "none")()
+        changeDisplay("#login_form_container", "block")()
+    });
+
+    $("#back_button").click(function (){
+        changeDisplay("#choose_auth_type", "block")()
+        changeDisplay("#login_form_container", "none")()
+    });
+
+    $("#login_form").submit(function (e) {
+        e.preventDefault();
+       alert("wqe");
     });
 }
