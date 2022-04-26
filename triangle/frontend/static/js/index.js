2a822806ad51da296809debc2963e0584a3721b2
Valeron01 <electronpochta3011@mail.ru>
2022-04-26 21:32:19 +0300

    Added confirm email window

--- triangle/frontend/static/js/index.js
+++ triangle/frontend/static/js/index.js
@@ -25,9 +25,25 @@ function ()
         changeDisplay("#login_form_container", "none")();
     });
 
-    $("#login_form").submit(function (e) {
+    $("#login_form").submit((e) => {
         e.preventDefault();
-       alert("wqe");
+        sessionStorage.clear();
+        let reg_password = $("#reg_password").val();
+
+        if (reg_password !== $("#reg_password_confirm").val()) {
+            alert("Пароли дожны совпадать!");
+            return;
+        } else if (reg_password.length < 8) {
+            alert("Пароль слишком короткий!");
+            return;
+        }
+
+        sessionStorage.setItem("login", $("#reg_login").val());
+        sessionStorage.setItem("email", $("#reg_email").val());
+        sessionStorage.setItem("password", reg_password);
+
+        changeDisplay("#confirm_email_window", "block")();
+        changeDisplay("#register_container", "none")();
     });
 
     $("#back_button_register").click(function () {
@@ -36,4 +52,13 @@ function ()
         console.log("Clicked bak register")
     });
 
+    $("#accept_code").submit((e) => {
+        e.preventDefault();
+
+    });
+
+}
+
+window.onclose = () => {
+    sessionStorage.clear();
 }
