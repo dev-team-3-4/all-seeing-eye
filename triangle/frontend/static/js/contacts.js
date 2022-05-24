eb1dd4cab83df6ae8847f6c524281915ee7a6b2c
Valeron01 <electronpochta3011@mail.ru>
2022-05-24 22:26:47 +0300

    Добавил поиск новых юзеров, скроллинг

--- triangle/frontend/static/js/contacts.js
+++ triangle/frontend/static/js/contacts.js
@@ -9,36 +9,43 @@ window.onload = () => {
             `   </div>\n` +
             `</li>`;
 
-            $(".chats_list").append(result);
+            $("#chats_list").append(result);
     }
 
     let users_page = 1;
     let scrolled_to_end = false;
 
     function scrolledToBottom() {
+        if (scrolled_to_end)
+            return;
+        $.ajax({
+            url: "/user/contacts/",
+            method: "GET",
+            dataType: "json",
+            data: {
+                page: users_page,
+                page_size: 10
+            },
+            success: (data) => {
+                total_users = data["count"]
+                data["results"].forEach((item) => {
+                    addContactToList(item["username"], item["profile_photo"] == null ? "/static/img/camera_400.gif" : item["profile_photo"]);
+                });
+                users_page += 1;
+                scrolled_to_end = total_users < (10 * (users_page - 1));
+                scrollEvent()
+            },
+            headers: {
+                "Authorization": "Token " + getCookie("token"),
+            }
+        });
+
+    }
+
+    function scrollEvent() {
         if (my_div.offsetHeight + my_div.scrollTop >= my_div.scrollHeight) {
-            if (scrolled_to_end)
-                return;
-            $.ajax({
-                url: "/user/contacts/",
-                method: "GET",
-                dataType: "json",
-                data: {
-                    page: users_page,
-                    page_size: 10
-                },
-                success: (data) => {
-                    total_users = data["count"]
-                    data["results"].forEach((item) => {
-                        addContactToList(item["username"], item["profile_photo"] == null ? "/static/img/camera_400.gif" : item["profile_photo"]);
-                    });
-                    users_page += 1;
-                    scrolled_to_end = total_users < (10 * users_page);
-                },
-                headers: {
-                    "Authorization": "Token " + getCookie("token"),
-                }
-            });
+            console.log("scrolled to end")
+            scrolledToBottom();
         }
     }
 
@@ -47,9 +54,9 @@ window.onload = () => {
 
     let my_div = document.getElementById("contacts_scroll");
 
-    scrolledToBottom();
+    scrollEvent();
 
     scroll_div.on('scroll', function() {
-        scrolledToBottom();
+        scrollEvent();
     });
 }
