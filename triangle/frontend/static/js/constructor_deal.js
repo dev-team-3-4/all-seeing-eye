1756d8be5452c23cd8fd916993fb206cec2a5bc3
Valeron01 <electronpochta3011@mail.ru>
2022-09-18 14:31:30 +0300

    Fixed sending number as string

--- triangle/frontend/static/js/constructor_deal.js
+++ triangle/frontend/static/js/constructor_deal.js
@@ -1,16 +1,19 @@
 window.onload = () => {
     $("#form_constructor").submit((e) => {
         e.preventDefault()
-        const contract_id = +location.href.substring(location.href.lastIndexOf("/") + 1)
-        $.ajax({
-            url: `/contract/${contract_id}/withdrawal/`,
-            method: "post",
-            data: {
+
+        let send_data = {
                 "first_user_funds": +$("#first_user_funds").val(),
                 "second_user_funds": +$("#second_user_funds").val(),
                 "moderator_funds": +$("#moderator_funds").val(),
                 "close_contract": $("#deal_closing_checkbox").is(":checked")
-            },
+            };
+        const contract_id = +location.href.substring(location.href.lastIndexOf("/") + 1)
+        $.ajax({
+            url: `/contract/${contract_id}/withdrawal/`,
+            method: "post",
+            data: JSON.stringify(send_data),
+            contentType: "application/json",
             success: (data) => {
                 let user_id = data["id"];
                 alert("Успешно")
